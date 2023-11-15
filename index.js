48f7e332d88786675ad4a25c37b5aeb0b4ab7265
Manisha Anantharam <manishaanantharam@Manishas-MBP.lan>
2023-11-14 23:50:19 -0600

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express(); // generates new function with app 
 
 app.get('/', (req,res) => {
-   res.send({ hi:'there' });
+   res.send({ bye : "buddy" });
 });
 
 const PORT = process.env.PORT || 5001;
